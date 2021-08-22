da194bbc4ce665138c90cd76948d9aa3451458ec
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-22 16:17:26 -0400

    Added animation

--- Music Video Webpage/Music Video Webpage/wwwroot/js/site.js
+++ Music Video Webpage/Music Video Webpage/wwwroot/js/site.js
@@ -1,4 +1,37 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var id = null;
 
-// Write your Javascript code.
+function myMove() {
+    console.log("got here")
+    var elem = document.getElementById("myAnimation");
+    var xpos = 0
+    var ypos = 0
+    var xvel = 1
+    var yvel = 1
+    clearInterval(id)
+    id = setInterval(frame, 10)
+    function frame() {
+        if (xpos >= 350) {
+            xpos = 350
+            xvel = -1 * Math.floor(Math.random()*5)
+        }
+        if (ypos >= 350) {
+            ypos = 350
+            yvel = -1 * Math.floor(Math.random()*5)
+        }
+        if (xpos <= 0) {
+            xpos = 0
+            xvel = Math.floor(Math.random() * 5)
+        }
+        if (ypos <= 0) {
+            ypos = 0
+            yvel = Math.floor(Math.random() * 5)
+        }
+        xpos += xvel
+        ypos += yvel
+        console.log(xpos, xvel, ypos, yvel)
+        elem.style.top = ypos + 'px'
+        elem.style.left = xpos + 'px'
+    }
+}
+
+document.getElementsByTagName("body")[0].addEventListener("load", myMove())
