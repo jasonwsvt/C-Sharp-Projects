3b550ba434914f3f325d971c24f3a62eaff10fa5
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-24 09:52:07 -0400

    Branching assignment

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express.  Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            float weight = float.Parse(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.  Have a good day.");
+            }
+            Console.WriteLine("Please enter the package width:");
+            float width = float.Parse(Console.ReadLine());
+            if (width > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.  Have a good day.");
+            }
+            Console.WriteLine("Please enter the package height:");
+            float height = float.Parse(Console.ReadLine());
+            if (width + height > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.  Have a good day.");
+            }
+            Console.WriteLine("Please enter the package length:");
+            float length = float.Parse(Console.ReadLine());
+            if (width + height + length > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.  Have a good day.");
+            }
+            Console.WriteLine("Your estimated total for shipping this package is: " + string.Format("{0:C}", height * width * length * weight / 100));
+            Console.WriteLine("Thank you!");
+        }
+    }
+}
