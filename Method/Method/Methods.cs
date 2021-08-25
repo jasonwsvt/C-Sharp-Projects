2b26d6dbefb3e5fd778246e511c6e074383b1250
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-25 17:47:36 -0400

    Method assignment

---
+++ Method/Method/Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method
+{
+    class Methods
+    {
+        //If the second parameter is not provided, give it a default value of 0.
+        public int AddTwo(int one, int two = 0)
+        {
+            //Return the sum of both parameters.
+            return one + two;
+        }
+    }
+}
---
+++ Method/Method/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Create a new Methods instance
+            Methods n = new Methods();
+            
+            //Print text to the console
+            Console.Write("Please enter a number: ");
+
+            //Read user-entered text, convert it to an integer, assign it to a variable
+            int first = int.Parse(Console.ReadLine());
+
+            //Print text to the console
+            Console.Write("Please enter a second number if you like: ");
+
+            //If an integer is entered, call the function with both variables,
+            try
+            {
+                //Read user-entered text, convert it to an integer, assign it to a variable
+                int second = int.Parse(Console.ReadLine());
+
+                //Print text to the console, ending with a call to a method of the Methods class.
+                Console.WriteLine("add(" + first + ", " + second +"): " + n.AddTwo(first, second));
+            }
+            //otherwise just call with the first.
+            catch
+            {
+                //Print text to the console, ending with a call to a method of the Methods class.
+                Console.WriteLine("add(" + first + "): " + n.AddTwo(first));
+            }
+        }
+    }
+}
