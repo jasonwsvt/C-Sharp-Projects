3602eb4ce678b7c4e4378e6819df715cbb009ea4
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-24 20:40:50 -0400

    Update

--- Strings and Integers/Strings and Integers/Program.cs
+++ Strings and Integers/Strings and Integers/Program.cs
@@ -14,7 +14,7 @@ namespace Strings_and_Integers
             Console.Write("Please enter an integer: ");
 
             //Assign an initial value to a variable of integer type.
-            int denominator = 0;
+            float denominator = 0f;
 
             //Begin a loop
             do
@@ -22,32 +22,33 @@ namespace Strings_and_Integers
                 //Begin a try / catch block
                 try
                 {
-                    //Convert user input to an integer and assign it to denominator.
-                    denominator = int.Parse(Console.ReadLine());
+                    //Convert user input to a float and assign it to denominator.
+                    denominator = float.Parse(Console.ReadLine());
 
                     //Catch the divide by zero error before it would occur.
-                    if (denominator == 0)
+                    if (denominator == 0f)
                     {
-                        Console.Write("Please enter a non-zero integer: ");
+                        Console.Write("Please enter a non-zero number: ");
                     }
                 }
-                //Catch the error on read that might occur if an integer wasn't entered.
+                //Catch the error on read that might occur if a number wasn't entered.
                 catch (FormatException)
                 {
-                    Console.Write("Please enter a valid integer: ");
+                    Console.Write("Please enter a valid number: ");
                 }
+                //Catch the error on read that might occur if the number is greater than 
                 catch (OverflowException)
                 {
-                    Console.Write("Please enter a reasonably sized integer: ");
+                    Console.Write("Please enter a number below " + float.MaxValue.ToString("#") + ": ");
                 }
             }
             //repeat until a valid number is entered.
-            while (denominator == 0);
+            while (denominator == 0f);
 
             //Print out the fraction of each number in the numList list divided by the entered denominator.
             foreach (ushort num in numList)
             {
-                Console.WriteLine(num + " / " + denominator + " = " + (float)num / denominator);
+                Console.WriteLine(num + " / " + denominator + " = " + num / denominator);
             }
         }
     }
