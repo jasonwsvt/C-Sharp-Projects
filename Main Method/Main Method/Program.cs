e77cbc1ed3dd2076cb7ad82e49d1080b939cb037
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-25 16:32:24 -0400

    Finish commenting

--- Main Method/Main Method/Program.cs
+++ Main Method/Main Method/Program.cs
@@ -6,10 +6,16 @@ namespace Main_Method
     {
         static void Main()
         {
+            //Create an instantiation of the MathOperations class.
             MathOperations mos = new MathOperations();
 
+            //Write the result of the integer RandFunc method to the console.
             Console.WriteLine("RandFunc(10): " + mos.RandFunc(10));
+
+            //Write the result of the decimal RandFunc method to the console.
             Console.WriteLine("RandFunc(34.1254453): " + mos.RandFunc(34.1254453m));
+
+            //Write the result of the string RandFunc method to the console.
             Console.WriteLine("RandFunc(\"300\"): " + mos.RandFunc("300"));
         }
     }
