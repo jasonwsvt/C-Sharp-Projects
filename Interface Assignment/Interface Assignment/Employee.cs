45fd667dc0c26029274f648590ed14c692db4fb5
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-26 14:37:55 -0400

    Update

--- Interface Assignment/Interface Assignment/Employee.cs
+++ Interface Assignment/Interface Assignment/Employee.cs
@@ -8,6 +8,8 @@ namespace Interface_Assignment
     //Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
     class Employee : Person, IQuittable
     {
+        public int Id;
+
         //Implement the SayName() method inside of the Employee class.
         public override void SayName()
         {
@@ -22,13 +24,13 @@ namespace Interface_Assignment
         //Overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property.
         public static bool operator ==(Employee one, Employee two)
         {
-            return (one.FirstName == two.FirstName && one.LastName == two.LastName);
+            return (one.Id == two.Id);
         }
 
         //Added below method as per Compiler Error CS0216
         public static bool operator !=(Employee one, Employee two)
         {
-            return (one.FirstName != two.FirstName || one.LastName != two.LastName);
+            return (one.Id != two.Id);
         }
 
         //Added both of the methods below as per Compiler Warning (level 3) CS0660
--- Interface Assignment/Interface Assignment/Program.cs
+++ Interface Assignment/Interface Assignment/Program.cs
@@ -7,10 +7,13 @@ namespace Interface_Assignment
         static void Main()
         {
             //Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”.
-            IQuittable sample = new Employee { FirstName = "Sample", LastName = "Student " };
+            Employee sample = new Employee { FirstName = "Sample", LastName = "Student", Id = 1 };
+            Employee sample2 = new Employee { FirstName = "Sample", LastName = "Student", Id = 2 };
 
             //Call the SayName() method on the object.
-            //sample.SayName();
+            sample.SayName();
+
+            Console.WriteLine("Employees are the same? " + (sample == sample2));
 
             //Call the IQuittable.Quit method from the employee.
             sample.Quit();
