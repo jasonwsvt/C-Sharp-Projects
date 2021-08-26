4ecea6f8c2404104f12ccc1bccb3cd744d23000f
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-26 11:20:37 -0400

    Void Method

---
+++ Method Class/Method Class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Method_Class
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Create an instance of VoidMethod
+            VoidMethod vm = new VoidMethod();
+
+            //Call the VoidMethod method with two parameters.
+            vm.VM(4, 5);
+
+            //Call the same method with named parameters.
+            vm.VM(two: 5, one: 4);
+        }
+    }
+}
---
+++ Method Class/Method Class/VoidMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Class
+{
+    class VoidMethod
+    {
+        public void VM(int one, int two)
+        {
+            //do a math operation on the first integer
+            one *= 2;
+
+            //Display the second integer to the screen.
+            Console.WriteLine(two);
+        }
+    }
+}
