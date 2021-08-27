054006b03e76f3274978447831dcd24137eb8920
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-26 21:55:38 -0400

    Enum Challenge

---
+++ Enum Challenge/Enum Challenge/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Enum_Challenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Please enter the day of the week: ");
+            Enum day;
+            bool valid = false;
+
+            //Loop until the day is valid.
+            do
+            {
+                //Wrap the above statement in a try/catch block.
+                try
+                {
+                    //Prompt the user to enter the current day of the week.
+                    //Assign the value to a variable of that enum data type you just created.
+                    day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine());
+                    valid = true;
+                }
+                catch
+                {
+                    //Have it print "Please enter an actual day of the week.” to the console if an error occurs.
+                    Console.Write("Please enter an actual day of the week: ");
+                }
+            }
+            while (!valid);
+        }
+
+        //Create an enum for the days of the week.
+        public enum DayOfWeek { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
+    }
+}
