b8c57c50cb05316a78f0438899f52e9024ee734f
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-29 15:24:58 -0400

    Chained constructors

---
+++ Constructor/Constructor/Chained.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor
+{
+    public class Chained
+    {
+        //Chain two constructors together.
+        public Chained(): this("chained")
+        {
+        }
+        public Chained(string s)
+        {
+            Str = s;
+        }
+        public string Str;
+    }
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(int num)
+        {
+            //Create a const variable.
+            const int constInt = 1;
+
+            //Create a variable using the keyword “var.”
+            var intList = new List<int>(constInt);
+
+            //Two instantiations of a class with chained constructors.
+            Chained chained1 = new Chained();
+            Console.WriteLine(chained1.Str);
+
+            Chained chained2 = new Chained("stuff");
+            Console.WriteLine(chained2.Str);
+        }
+    }
+}
