328aaf5386fa265d468a495ae05e1b16c8c670ab
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-09-01 16:19:07 -0400

    CS0012 error

--- CarInsurance/CarInsurance/Controllers/InsureeController.cs
+++ CarInsurance/CarInsurance/Controllers/InsureeController.cs
@@ -46,12 +46,6 @@ namespace CarInsurance.Controllers
             return View(db.Insurees.ToList());
         }
 
-        // GET: Insuree
-        public ActionResult Admin()
-        {
-            return View(db.Insurees.ToList());
-        }
-
         // GET: Insuree/Details/5
         public ActionResult Details(int? id)
         {
