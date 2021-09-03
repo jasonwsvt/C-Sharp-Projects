fde0f569bdbc2eb99937dc94502c1ff5124a11a9
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-09-03 18:42:01 -0400

    Final Challenge

---
+++ Final Challenge 2/Final Challenge 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Challenge_2
+{
+	class Program
+	{
+		static void Main()
+		{
+			using (var ctx = new SchoolContext())
+			{
+				var s = new Student() { StudentName = "Jason" };
+				ctx.Students.Add(s);
+				ctx.SaveChanges();
+				Console.WriteLine("Number of students: " + ctx.Students.Count());
+			}
+		}
+	}
+
+	public class Student
+	{
+		public int StudentId { get; set; }
+		public string StudentName { get; set; }
+		public DateTime? DateOfBirth { get; set; }
+		public int ClassYear { get; set; }
+	}
+
+	public class SchoolContext: DbContext
+	{
+		public SchoolContext() : base()
+		{
+
+		}
+
+		public DbSet<Student> Students { get; set; }
+	}
+}
---
+++ Final Challenge/Final Challenge/DataAccess/StudentData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final_Challenge.DataAccess
+{
+	public class Student : DbContext
+	{
+		public Student StudentContext(): base()
+		{
+			Student s = new();
+
+			return s;
+		}
+
+		public DbSet<Student> Students { get; set; }
+
+	}
+}
